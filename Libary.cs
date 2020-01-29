a67d6dcebd1dc1802d32d39a7523f87a9cf20711
jerilyn992 <60419096+jerilyn992@users.noreply.github.com>
2020-01-28 21:45:29 -0800

    Add files via upload

---
+++ Libary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace BookStoreConsole
+{
+    class Libary
+    {
+        public string BookName { get; set; }
+        public string BookAuthor { get; set; }
+        public decimal BookCost { get; set; }
+        public string BookType { get; set; }
+
+        public string BookCategcory { get; set; }
+    }
+}
